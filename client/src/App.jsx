5fdaf155abdbb1cfcc87db0296477f84ac551500
ataberkakkan <akkanataberk@gmail.com>
2025-05-21 14:36:25 +0300

    Admin Dashboard Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import { RouterProvider } from "react-router";
 import Courses from "./pages/student/Courses";
 import MyLearning from "./pages/student/MyLearning";
 import Profile from "./pages/student/Profile";
+import Sidebar from "./pages/admin/Sidebar";
+import Dashboard from "./pages/admin/Dashboard";
+import CourseTable from "./pages/admin/course/CourseTable";
+import CreateCourse from "./pages/admin/course/CreateCourse";
 
 const appRouter = createBrowserRouter([
   {
@@ -22,16 +26,36 @@ const appRouter = createBrowserRouter([
         ),
       },
       {
-        path: "/login",
+        path: "login",
         element: <Login />,
       },
       {
-        path: "/my-learning",
+        path: "my-learning",
         element: <MyLearning />,
       },
       {
-        path: "/profile",
+        path: "profile",
         element: <Profile />,
+      },
+
+      // ADMIN ROUTES
+      {
+        path: "admin",
+        element: <Sidebar />,
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "courses",
+            element: <CourseTable />,
+          },
+          {
+            path: "courses/create",
+            element: <CreateCourse />,
+          },
+        ],
       },
     ],
   },
